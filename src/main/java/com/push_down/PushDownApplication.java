3bd036090815a32a9e8b9ecef968277ae720903b
valisoa01 <valisoatolotriniaina@gmail.com>
2026-02-18 15:30:34 +0300

    first commit

---
+++ src/main/java/com/push_down/PushDownApplication.java
@@ -0,0 +1,13 @@
+package com.push_down;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushDownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushDownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/push_down/PushDownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.push_down;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushDownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
